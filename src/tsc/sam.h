3c4dc91cfec9ad046f94adf4c132be7a11f69fdf
Jan Voges <voges@tnt.uni-hannover.de>
2021-03-03 16:55:40 +0100

    Fix compilation errors on Ubuntu 16.04 and MacOS

--- src/tsc/sam.h
+++ src/tsc/sam.h
@@ -1,20 +1,20 @@
 #include <fstream>
 #include <vector>
 
-class SamRecord {
-    std::string qname;  // Query template NAME
-    uint16_t    flag;   // bitwise FLAG (uint16_t)
-    std::string rname;  // Reference sequence NAME
-    uint32_t    pos;    // 1-based leftmost mapping POSition (uint32_t)
-    uint8_t     mapq;   // MAPping Quality (uint8_t)
-    std::string cigar;  // CIGAR string
-    std::string rnext;  // Ref. name of the mate/NEXT read
-    uint32_t    pnext;  // Position of the mate/NEXT read (uint32_t)
-    int64_t     tlen;   // observed Template LENgth (int64_t)
-    std::string seq;    // segment SEQuence
-    std::string qual;   // QUALity scores
-    std::string opt;    // OPTional information
-};
+// class SamRecord {
+//     std::string qname;  // Query template NAME
+//     uint16_t    flag;   // bitwise FLAG (uint16_t)
+//     std::string rname;  // Reference sequence NAME
+//     uint32_t    pos;    // 1-based leftmost mapping POSition (uint32_t)
+//     uint8_t     mapq;   // MAPping Quality (uint8_t)
+//     std::string cigar;  // CIGAR string
+//     std::string rnext;  // Ref. name of the mate/NEXT read
+//     uint32_t    pnext;  // Position of the mate/NEXT read (uint32_t)
+//     int64_t     tlen;   // observed Template LENgth (int64_t)
+//     std::string seq;    // segment SEQuence
+//     std::string qual;   // QUALity scores
+//     std::string opt;    // OPTional information
+// };
 
 class SamParser {
 public:
