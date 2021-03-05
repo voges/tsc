b05717ef3ed9c80094ac58de5d15f8ea1955fd52
Jan Voges <voges@tnt.uni-hannover.de>
2021-03-05 18:33:42 +0100

    Update scripts/test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,6 +3,6 @@
 git rev-parse --git-dir 1>/dev/null || exit 1
 readonly git_root_dir="$(git rev-parse --show-toplevel)"
 readonly build_dir="${git_root_dir}/build"
-"${build_dir}/bin/rans_test"
-"${build_dir}/bin/sam_test"
-"${build_dir}/bin/zlib_wrapper_test"
+"${build_dir}/bin/rans_tests"
+"${build_dir}/bin/tsc_tests"
+"${build_dir}/bin/zlib_wrapper_tests"
